3a4304d76cfd7783ca24c2baa9f8ba6896d07158
Rogier van Staveren <rogier@batoid.com>
2020-09-21 20:22:54 +0100

    first commit

---
+++ IAmHere/__init__.py
@@ -0,0 +1,251 @@
+import sys, logging, signal, time, urllib.request, json, os, platform, subprocess, re
+
+ipLocationConfigs = {
+  'ip-api.com': { 'url': 'http://ip-api.com/json/?fields=49344', 'latitudeKey': 'lat', 'longitudeKey': 'lon', 'interval': 60/45},
+  'ipapi.co': { 'url': 'https://ipapi.co/json/', 'latitudeKey': 'latitude', 'longitudeKey': 'longitude', 'interval': (60*60*24)/1000},
+  'extreme-ip-lookup.com': { 'url': 'https://extreme-ip-lookup.com/json/', 'latitudeKey': 'lat', 'longitudeKey': 'lon', 'interval': (60*60*24*31)/10000},
+  'ipwhois.app': { 'url': 'https://ipwhois.app/json/?objects=latitude,longitude', 'latitudeKey': 'latitude', 'longitudeKey': 'longitude', 'interval': (60*60*24*31)/10000}
+}
+
+coreLocation = False
+if platform.system() == 'Darwin':
+  coreLocation = True
+locationService = False
+if platform.system() == 'Windows':
+  locationService = True
+wifiLocationLookup = True
+ipLocationLookup = True
+ipLocationProvider = 'ip-api.com'
+ipLocationConfig = None
+
+terminate = False
+gpsd = None
+url = None
+interval = 5
+
+previousBSSID = None
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+try:
+  from gps import *
+except ImportError:
+  logger.warning("Python GPSd library not installed")
+
+def stop(signum = None, frame = None):
+  """ Stop the IAmHere process """
+  global terminate
+
+  logger.info("Stopping IAmHere process ")
+  terminate = True
+
+def run():
+  """ The main loop of the IAmHere process """
+  global terminate
+  global gpsd
+  global url
+  global interval
+  global wifiLocationLookup
+  global ipLocationConfigs, ipLocationConfig, ipLocationProvider, ipLocationLookup
+
+  if 'gps' in sys.modules:
+    try:
+      gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+    except ConnectionRefusedError as e:
+      logger.warning("Can't connect to GPSd")
+
+  ipLocationConfig = ipLocationConfigs[ipLocationProvider]
+
+  previousLocation = None
+  _time = 0
+  while not terminate:
+    location = None
+
+    elapsedTime = time.time() - _time
+    #logger.debug(elapsedTime)
+    if elapsedTime > interval:
+      if gpsd:
+        location = getGPSLocation()
+        if location:
+          logger.debug(location)
+          _time = time.time()
+
+      if not location and coreLocation:
+        location = getCoreLocationLocation()
+        if location:
+          logger.debug(location)
+          _time = time.time()
+
+      if not location and locationService:
+        location = getLocationServiceLocation()
+        if location:
+          logger.debug(location)
+          _time = time.time()
+
+      if not location and wifiLocationLookup:
+        location = getWiFiLocation()
+        if location:
+          logger.debug(location)
+          _time = time.time()
+
+    if not location and ipLocationLookup and elapsedTime > interval and elapsedTime > ipLocationConfig['interval']:
+      location = getIPLocation()
+      if location:
+        logger.debug(location)
+        _time = time.time()
+
+    if location and location != previousLocation:
+      try:
+        logger.debug('{}?la={}&lo={}'.format(url, location[0], location[1]))
+        urllib.request.urlopen('{}?la={}&lo={}'.format(url, location[0], location[1]), timeout=1)
+        previousLocation = location
+      except urllib.error.URLError as e:
+        logger.error(e.reason)
+      except urllib.error.HTTPError as e:
+        logger.error(e.code)
+
+    time.sleep(0.1)
+
+  logger.info("Stopped IAmHere daemon")
+
+def getGPSLocation():
+  """ Get the location using the GPS daemon """
+  global gpsd
+
+  if gpsd:
+    _time = time.time()
+    while not terminate:
+      if gpsd.waiting():
+        # Get current location from GPS device
+        report = gpsd.next()
+        if report['class'] == 'TPV':
+          if report['mode'] in [0, 1]:
+            return None
+
+          if 'lat' in report and 'lon' in report:
+            return [ report['lat'], report['lon'] ]
+
+          logger.debug(report)
+          return None
+        elif report['class'] == 'DEVICES':
+          if len(report['devices']) == 0:
+            logger.warning("No GPS device connected")
+            return None
+          _time = time.time()
+        elif report['class'] == 'SKY':
+          if 'satellites' not in report:
+            logger.debug("No satellites in view")
+            return None
+          _time = time.time()
+        else:
+          logger.debug(report)
+          _time = time.time()
+      else:
+        elapsedTime = time.time() - _time
+        logger.debug(elapsedTime)
+        if elapsedTime > 1:
+          return None
+        time.sleep(0.1)
+
+  return None
+
+def getCoreLocationLocation():
+  """ Get the location using the macOS Core Location API """
+  if platform.system() == 'Darwin':
+    #ToDo
+    return None
+
+  logger.debug("Core Location not supported")
+  return None
+
+def getLocationServiceLocation():
+  """ Get the location using the Windows Location Service API """
+  if platform.system() == 'Windows':
+    #ToDo
+    return None
+
+  logger.debug("Location Service not supported")
+  return None
+
+def getWiFiLocation():
+  """ Get the location using the WiFi BSSID """
+  global previousBSSID
+
+  bssid = None
+  
+  if platform.system() == 'Linux':
+    # Get the default route interface
+    proc = subprocess.Popen(['ip', 'route', 'show', 'default'], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    proc.wait()
+    output = proc.stdout.read().decode()
+
+    interfaces = re.findall("^default via [0-9.]* dev ([^ ]*)", output, re.MULTILINE)
+    interfaces = list(set(interfaces))
+
+    if len(interfaces) != 1:
+      logger.warning("More than one default route interfaces detected")
+      return None
+
+    interface = interfaces[0]
+    proc = subprocess.Popen(['iwconfig', interface], stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+    proc.wait()
+    output = proc.stdout.read().decode()
+    bssids = re.findall("Access Point: ([0-9a-fA-F:]*)", output, re.MULTILINE)
+
+    signals = re.findall("Signal level=(-[0-9]*) dBm", output, re.MULTILINE)
+
+    if len(bssids) != 1:
+      logger.warning("More than one AP BSSID detected")
+      return None
+
+    if len(signals) != 1:
+      logger.warning("More than one AP BSSID detected")
+      return None
+
+    bssid = bssids[0]
+    signal = signals[0]
+  elif platform.system() == 'Darwin':
+    #ToDo
+    return None
+  elif platform.system() == 'Windows':
+    #ToDo
+    return None
+
+  if bssid and bssid != previousBSSID:
+    _bssid = bssid.replace(':', '')
+    url = "http://mobile.maps.yandex.net/cellid_location/?wifinetworks={}:{}".format(_bssid, signal)
+    logger.debug(url)
+    try:
+      with urllib.request.urlopen(url, timeout=1) as response:
+        data = response.read().decode('utf-8')
+        logger.debug(data)
+        latitude = float(re.compile(" latitude=\"([0-9.]*)\".*", re.MULTILINE).search(response).group(1))
+        longitude = float(re.compile(" longitude=\"([0-9.]*)\".*", re.MULTILINE).search(response).group(1))
+        return [latitude, longitude]
+    except urllib.error.HTTPError as e:
+      if e.code == 404:
+        logger.debug("Location not found for BSSID {}".format(bssid))
+      else:
+        logger.error(e.code)
+    except urllib.error.URLError as e:
+      logger.error(e)
+    previousBSSID = bssid
+
+  return None
+
+def getIPLocation():
+  """ Get the location using the external IP address """
+  global ipLocationConfig
+
+  try:
+    with urllib.request.urlopen(ipLocationConfig['url'], timeout=1) as response:
+      data = response.read().decode('utf-8')
+      data = json.loads(data)
+      return [ data.get(ipLocationConfig['latitudeKey']), data.get(ipLocationConfig['longitudeKey']) ]
+  except urllib.error.URLError as e:
+    logger.error(e.reason)
+  except urllib.error.HTTPError as e:
+    logger.error(e.code)
+
+  return None
---
+++ IAmHere/__main__.py
@@ -0,0 +1,84 @@
+import os, logging, signal, argparse
+import __init__ as IAmHere
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+def daemonize():
+  # Example code from https://gist.github.com/slor/5946334
+
+  # fork 1 to spin off the child that will spawn the daemon
+  if os.fork():
+    sys.exit()
+
+  # This is the child.
+  # 1. cd to root for a guarenteed working dir
+  # 2. clear the session id to clear the controlling TTY
+  # 3. set the umask so we have access to all files created by the daemon
+  os.chdir("/")
+  os.setsid()
+  os.umask(0)
+
+  # fork 2 ensures we can't get a controlling ttd.
+  if os.fork():
+    sys.exit()
+
+  # This is a child that can't ever have a controlling TTY.
+  # Now we shut down stdin and point stdout/stderr at log files.
+
+  # stdin
+  with open('/dev/null', 'r') as dev_null:
+    os.dup2(dev_null.fileno(), sys.stdin.fileno())
+
+  # stderr - do this before stdout so that errors about setting stdout write to the log file.
+  #
+  # Exceptions raised after this point will be written to the log file.
+  sys.stderr.flush()
+  with open(stderr, 'a+') as stderr:
+    os.dup2(stderr.fileno(), sys.stderr.fileno())
+
+  # stdout
+  #
+  # Print statements after this step will not work. Use sys.stdout
+  # instead.
+  sys.stdout.flush()
+  with open(stdout, 'a+') as stdout:
+    os.dup2(stdout.fileno(), sys.stdout.fileno())
+
+if __name__ == '__main__':
+  # Read command line arguments
+  argparser = argparse.ArgumentParser()
+  argparser.add_argument('url')
+  argparser.add_argument("-f", "--foreground", dest="foreground", action="store_const", const=True, default=False, help="Run process in the foreground")
+  argparser.add_argument("-i", "--interval", dest="interval", type=int, default=IAmHere.interval, help="Logging interval in seconds (default: %(default)s)")
+  argparser.add_argument("--nowifi", "--nowifilocationlookup", dest="wifiLocationLookup", action="store_const", const=False, default=True, help="Don't fall back to WiFi AP location lookup if other methods are unsuccessful")
+  argparser.add_argument("--noip", "--noiplocationlookup", dest="ipLocationLookup", action="store_const", const=False, default=True, help="Don't fall back to IP location lookup if other methods are unsuccessful")
+  argparser.add_argument("--iplocationprovider", dest="ipLocationProvider", choices=IAmHere.ipLocationConfigs.keys(), default=IAmHere.ipLocationProvider, help="Provider for IP location lookup (default: %(default)s)")
+  args = argparser.parse_args()
+
+  if args.foreground:
+    logging.basicConfig(format='%(levelname)-8s %(message)s')
+  #else:
+  #  logging.basicConfig(filename=logFile, format='%(asctime)s %(levelname)-8s %(name)s.%(funcName)s() %(message)s', datefmt='%x %X', level=logging.INFO)
+
+  logger.info("Starting IAmHere")
+
+  signal.signal(signal.SIGINT, IAmHere.stop)
+  signal.signal(signal.SIGTERM, IAmHere.stop)
+
+  IAmHere.url = args.url
+  if args.interval:
+    IAmHere.interval = args.interval
+  IAmHere.wifiLocationLookup = args.wifiLocationLookup
+  IAmHere.ipLocationLookup = args.ipLocationLookup
+  if args.ipLocationProvider:
+    IAmHere.ipLocationProvider = args.ipLocationProvider
+
+  if args.foreground:
+    try:
+      IAmHere.run()
+    except (KeyboardInterrupt):
+      IAmHere.stop()
+  else:
+    daemonize()
+    IAmHere.run()
